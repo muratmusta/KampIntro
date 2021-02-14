d4147fc6ed1a835aa887a344ab5ae3616401be68
MUSTAFA MURAT <m.muratblbl@gmail.com>
2021-02-14 06:38:22 +0300

    14.02.2021 Interface çalışması

---
+++ Interfaces/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //interface new'lenemez!
+            //interface leri şablon oluşturmak için kullanıyoruz.
+            //interface ler onu implemente eden class' ın referans numarasını tutabilirler.
+
+
+            //IPersonManager customerManager = new CustomerManager();
+            //IPersonManager employeeManager = new EmployeeManager();
+            ProjectManager projectManager = new ProjectManager();
+            projectManager.Add(new InternManager());
+        }
+
+
+        //interface lerde buradaki person da üyeler bırakabiliyorduk. Ama interface lerder üyeler oluyor ama üyelerin içi boi oluyor.
+
+
+        interface IPersonManager
+
+        {
+            //implemented operation yani tamamlanmış operasyon, yani implemente edilmiş, içi doldurulmuş operasyon demek oluyor.
+            //unimplemented operation
+            //interface üyeleri, dışarıdan erişilebilir olmalıdır.  O yüzden public yazamayız.
+            void Add(); //imza
+            void Update();
+        
+        }
+
+        //Eğer class olsaydı inherits...... implements - interface
+        class CustomerManager : IPersonManager
+
+        {
+            public void Add()
+            {
+                //Müşteri ekleme kodları yazılır.
+                Console.WriteLine("Müşteri eklendi");
+            }
+            public void Update()
+            {
+                //Müşteri ekleme kodları yazılır.
+                Console.WriteLine("Müşteri güncellendi");
+            }
+        }
+
+        class EmployeeManager : IPersonManager
+
+        {
+            public void Add()
+            {
+                //personel ekleme kodları yazılır.
+                Console.WriteLine("Personel eklendi");
+            }
+            public void Update()
+            {
+                //Müşteri ekleme kodları yazılır.
+                Console.WriteLine("Personel güncellendi");
+            }
+
+        }
+    }
+
+    class InternManager : IPersonManager
+    {
+        public void Add()
+        {
+
+            Console.WriteLine("Stajyer eklendi");
+
+        }
+
+        public void Update()
+        {
+
+            Console.WriteLine("Stajyer güncellendi");
+
+        }
+
+    }
+
+
+    class ProjectManager
+    {
+        public void Add(IPersonManager personManager)
+        
+        {
+            personManager.Add();
+        }
+    }
+    
+}
---
+++ InterfacesDemo/Program.cs
@@ -0,0 +1,101 @@
+﻿using InterfacesDemo;
+using System;
+
+namespace InterfacesDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWorker[] workers = new IWorker[3]
+            {
+                new Manager(),
+                new Worker(),
+                new Robot()
+
+            };
+
+            foreach (var worker in workers)
+            {
+                worker.Work();
+            }
+
+            IEat[] eats = new IEat[2]
+
+            {
+                new Worker(),
+                new Manager()
+
+            };
+
+            foreach (var eat in eats)
+            {
+                eat.Eat();
+            }
+        }
+
+
+    }
+
+    interface IWorker
+
+    {
+        void Work();
+    }
+    interface IEat
+
+    {
+        void Eat();
+    }
+
+    interface ISalary
+    {
+        void GetSalary();
+    }
+       
+
+    }
+
+class Manager : IWorker, IEat, ISalary
+{
+    public void Eat()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void GetSalary()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Work()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+class Worker : IWorker, IEat, ISalary
+{
+    public void Eat()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void GetSalary()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Work()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+class Robot : IWorker
+{
+    public void Work()
+    {
+       
+    }
+}
